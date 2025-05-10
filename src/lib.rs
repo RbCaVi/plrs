ddedc168c64e673a88b5a9da4efa4de143ec228c
RbCaViTerminal <robert@robertvail.info>
2025-05-10 19:52:06 -0400

    couple of functions on PvString

--- src/lib.rs
+++ src/lib.rs
@@ -74,9 +74,137 @@ impl std::ops::Rem for PvInt {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Copy, Clone)]
+struct PvStringData {
+    refcount: usize,
+    len: usize,
+    alloc_size: usize,
+}
+
+#[derive(Debug)]
 struct PvString {
     data: *mut PvStringData,
+}
+
+fn get_string_layout(size: usize) -> std::alloc::Layout {
+    let layout = std::alloc::Layout::new::<PvStringData>();
+    // no errors need to be handled
+    // the total size does not overflow isize (i'm assuming this one)
+    let (layout, _) = layout.extend(
+        // align (1) is not zero
+        // align (1) is a power of two
+        // size (size) does not overflow isize when rounded up to align (i'm assuming this one)
+        std::alloc::Layout::from_size_align(size, 1).unwrap()
+    ).unwrap();
+    layout
+}
+
+impl PvString {
+    // allocates enough space for `len` bytes of string
+    pub fn new_empty_sized(size: usize) -> Self {
+        let layout = get_string_layout(size);
+
+        let data = unsafe {std::alloc::alloc(layout)} as *mut PvStringData;
+
+        unsafe {*data = PvStringData {refcount: 1, len: 0, alloc_size: size};}
+
+        PvString {data}
+    }
+
+    pub fn new_empty() -> Self {
+        PvString::new_empty_sized(16) // any size would work
+    }
+
+    pub fn new(str: &str) -> Self {
+        let out = PvString::new_empty_sized(str.len());
+        unsafe {
+            (*out.data).len = str.len();
+            out.get_data_mut().copy_from_slice(str.as_bytes());
+        }
+        out
+    }
+
+    // allocates enough space for `len` bytes of string
+    unsafe fn resize(mut self, newsize: usize) {
+        let data = *self.data;
+
+        assert!(data.refcount == 1); // just a suggestion
+        assert!(newsize >= data.len); // just a suggestion
+
+        let oldlayout = get_string_layout(data.alloc_size);
+
+        let newlayout = get_string_layout(newsize);
+        
+        self.data = std::alloc::realloc(self.data as *mut u8, oldlayout, newlayout.size()) as *mut PvStringData;
+
+        (*self.data).alloc_size = newsize;
+    }
+
+    // only use when refcount = 1
+    unsafe fn get_data_mut(&self) -> &mut [u8] {
+        let data = unsafe {*self.data};
+
+        assert!(data.refcount == 1); // just a suggestion
+
+        let layout = std::alloc::Layout::new::<PvStringData>();
+        // no errors need to be handled
+        // the total size does not overflow isize (i'm assuming this one)
+        let (_, offset) = layout.extend(
+            // align (1) is not zero
+            // align (1) is a power of two
+            // size (data.len) does not overflow usize when rounded up to align (i'm assuming this one)
+            std::alloc::Layout::from_size_align(data.len, 1).unwrap()
+        ).unwrap();
+        // i could probably just get the size of layout
+        // but just to be safe :)
+
+        std::slice::from_raw_parts_mut(
+            (self.data as *mut u8).add(offset),
+            data.len
+        )
+    }
+
+    // get an immutable reference to the string data
+    // only use when refcount = 1
+    pub fn get_str(&self) -> &str {
+        // maybe use std::str::from_utf8_unchecked()?
+        unsafe {std::str::from_utf8(self.get_data_mut()).unwrap()}
+    }
+}
+
+macro_rules! decref {
+    ($pv:expr) => (unsafe {
+        (*$pv.data).refcount -= 1;
+        (*$pv.data).refcount
+    } == 0)
+}
+
+macro_rules! incref {
+    ($pv:expr) => (unsafe {(*$pv.data).refcount += 1})
+}
+
+impl Drop for PvString {
+    fn drop(&mut self) {
+        if (decref!(self)) {
+            let size = unsafe {(*self.data).alloc_size};
+            let layout = get_string_layout(size);
+
+            unsafe {std::alloc::dealloc(self.data as *mut u8, layout);}
+        }
+    }
+}
+
+impl Clone for PvString {
+    fn clone(&self) -> Self {
+        incref!(self);
+        PvString {data: self.data}
+    }
+}
+
+impl PartialEq for PvString {
+    fn eq(&self, other: &PvString) -> bool {
+        self.get_str() == other.get_str()
+    }
 }
 
 #[derive(PartialEq, Debug, Clone)]
@@ -239,5 +367,20 @@ mod tests {
     #[test]
     fn test_add_invalid() {
         assert_eq!(Pv::int(15) + Pv::bool(true), Pv::invalid());
+    }
+
+    #[test]
+    fn test_str_empty_constructor() {
+        PvString::new_empty();
+    }
+
+    #[test]
+    fn test_str_constructor() {
+        PvString::new("string");
+    }
+
+    #[test]
+    fn test_str_eq() {
+        assert_eq!(PvString::new("string"), PvString::new("string"));
     }
 }
