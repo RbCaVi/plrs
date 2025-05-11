7b4e35819e8b3ee0f791f5d52ca1ddeb07a8eb1f
RbCaViTerminal <robert@robertvail.info>
2025-05-11 18:21:49 -0400

    add "PvArray" (currently a copy of PvString)

--- src/lib.rs
+++ src/lib.rs
@@ -224,6 +224,167 @@ impl std::ops::Add<&PvString> for PvString {
     }
 }
 
+#[derive(Copy, Clone)]
+struct PvArrayData {
+    refcount: usize,
+    len: usize,
+    alloc_size: usize,
+}
+
+#[derive(Debug)]
+struct PvArray {
+    data: *mut PvArrayData,
+}
+
+impl PvArray {
+    // get a Layout that fits a PvArrayData with `size` bytes after it
+    fn get_layout(size: usize) -> std::alloc::Layout {
+        let layout = std::alloc::Layout::new::<PvArrayData>();
+        // no errors need to be handled
+        // the total size does not overflow isize (i'm assuming this one)
+        let (layout, _) = layout.extend(
+            // align (1) is not zero
+            // align (1) is a power of two
+            // size (size) does not overflow isize when rounded up to align (i'm assuming this one)
+            std::alloc::Layout::from_size_align(size, 1).unwrap()
+        ).unwrap();
+        layout
+    }
+
+    // allocates enough space for `len` bytes of string
+    pub fn new_empty_sized(size: usize) -> Self {
+        let layout = get_string_layout(size);
+
+        let data = unsafe {std::alloc::alloc(layout)} as *mut PvArrayData;
+
+        unsafe {*data = PvArrayData {refcount: 1, len: 0, alloc_size: size};}
+
+        PvArray {data}
+    }
+
+    pub fn new_empty() -> Self {
+        PvArray::new_empty_sized(16) // any size would work
+    }
+
+    pub fn new(str: &str) -> Self {
+        let out = PvArray::new_empty_sized(str.len() * 2); // any >= str.len()
+        unsafe {
+            (*out.data).len = str.len();
+        }
+        out.get_data_mut().copy_from_slice(str.as_bytes());
+        out
+    }
+
+    // move one copy of this string out and resize its allocation
+    // will reuse the old allocation if possible
+    unsafe fn resize_move(mut self, newsize: usize) -> Self {
+        let data = *self.data;
+
+        if data.refcount == 1 {
+            assert!(newsize >= data.len); // just a suggestion
+
+            let oldlayout = get_string_layout(data.alloc_size);
+
+            let newlayout = get_string_layout(newsize);
+            
+            self.data = std::alloc::realloc(self.data as *mut u8, oldlayout, newlayout.size()) as *mut PvArrayData;
+
+            (*self.data).alloc_size = newsize;
+
+            self
+        } else {
+            let out = PvArray::new_empty_sized(newsize);
+            (*out.data).len = data.len;
+            out.get_data_mut().copy_from_slice(self.get_data_mut());
+            out
+        }
+    }
+
+    // get a mutable slice reference to the string data
+    // only use when refcount = 1
+    fn get_data_mut(&self) -> &mut [u8] {
+        let data = unsafe {*self.data};
+
+        let layout = std::alloc::Layout::new::<PvArrayData>();
+        // no errors need to be handled
+        // the total size does not overflow isize (i'm assuming this one)
+        let (_, offset) = layout.extend(
+            // align (1) is not zero
+            // align (1) is a power of two
+            // size (data.len) does not overflow usize when rounded up to align (i'm assuming this one)
+            std::alloc::Layout::from_size_align(data.len, 1).unwrap()
+        ).unwrap();
+        // i could probably just get the size of layout
+        // but just to be safe :)
+
+        unsafe {
+            std::slice::from_raw_parts_mut(
+                (self.data as *mut u8).add(offset),
+                data.len
+            )
+        }
+    }
+
+    // get an immutable reference to the string data
+    // only use when refcount = 1
+    fn get_str(&self) -> &str {
+        // maybe use std::str::from_utf8_unchecked()?
+        // (because the "only" way to get a PvArray is from a &str)
+        std::str::from_utf8(self.get_data_mut()).unwrap()
+    }
+
+    pub fn concat(self, other: &PvArray) -> Self {
+        let data = unsafe {*self.data};
+        let otherdata = unsafe {*other.data};
+
+        let s = if data.refcount == 1 && data.alloc_size >= data.len + otherdata.len {
+            self
+        } else {unsafe {
+            self.resize_move((data.len + otherdata.len) * 2)
+        }};
+
+        unsafe {
+            (*s.data).len += otherdata.len;
+        }
+
+        s.get_data_mut()[data.len..].copy_from_slice(other.get_data_mut());
+
+        s
+    }
+}
+
+impl Drop for PvArray {
+    fn drop(&mut self) {
+        if (decref!(self)) {
+            let size = unsafe {(*self.data).alloc_size};
+            let layout = get_string_layout(size);
+
+            unsafe {std::alloc::dealloc(self.data as *mut u8, layout);}
+        }
+    }
+}
+
+impl Clone for PvArray {
+    fn clone(&self) -> Self {
+        incref!(self);
+        PvArray {data: self.data}
+    }
+}
+
+impl PartialEq for PvArray {
+    fn eq(&self, other: &PvArray) -> bool {
+        self.get_str() == other.get_str()
+    }
+}
+
+impl std::ops::Add<&PvArray> for PvArray {
+    type Output = Self;
+
+    fn add(self, other: &PvArray) -> Self {
+        self.concat(other)
+    }
+}
+
 #[derive(PartialEq, Debug, Clone)]
 enum Pv {
     Invalid(PvInvalid),
@@ -231,6 +392,7 @@ enum Pv {
     Bool(PvBool),
     Int(PvInt),
     String(PvString),
+    Array(PvArray),
 }
 
 impl Pv {
@@ -343,6 +505,7 @@ pvfrom!(Null PvNull);
 pvfrom!(Bool PvBool);
 pvfrom!(Int PvInt);
 pvfrom!(String PvString);
+pvfrom!(Array PvArray);
 
 macro_rules! unref_op_impl {
     ($type1:ident $type2:ident $optrait:ident $op:ident) => {
